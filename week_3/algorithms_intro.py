b216996ef126d8fe5950d5ac093d63614456f196
Benjamin Kwame Ampah <ampahben3@gmail.com>
2023-09-05 17:07:57 +0000

    second commit

--- week_3/algorithms_intro.py
+++ week_3/algorithms_intro.py
@@ -1,8 +1,22 @@
 pin = 1365
 balance = 50000
-input_pin = int(input("Please enter your PIN: "))
+number_of_times
 
-if input_pin == pin:
-    print(balance)
-else:
-    print("Please enter the correct PIN!")
+input_pin = int(input("Please enter your PIN: "))
+while (number_of_times > 0):
+    if (not input_pin.isdigit()):
+        print("Inavlid input, please enter a valid PIN number!")
+        break
+    if (pin == int(input_pin)):
+        print("Your balance is " + str(balance))
+        break
+    else:
+        number_of_times -= 1
+        # guard clause
+        if (number_of_times == 0):
+            break
+        input_pin = input("Incorrect PIN! Please try again!")
+# if input_pin == pin:
+#     print(balance)
+# else:
+#     print("Please enter the correct PIN!")
