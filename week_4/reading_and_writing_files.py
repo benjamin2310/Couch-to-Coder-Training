52cb92d579061d50cb383be8abb57cbc406a4f4e
Benjamin Kwame Ampah <ampahben3@gmail.com>
2023-09-22 00:25:32 +0000

    added week 4 folder

--- week_4/reading_and_writing_files.py
+++ week_4/reading_and_writing_files.py
@@ -4,4 +4,8 @@ books = []
 
 # opening the file
 with open("bestsellers with categories.csv", "r", encoding='utf-8', errors='replace') as csvfile:
-    print(csvfile)
+    # to print a csv you need to create a reader object
+    reader = csv.reader(csvfile, skipinitialspace=True)
+    # print the content of the csv file
+    for row in reader:
+        print(row)
